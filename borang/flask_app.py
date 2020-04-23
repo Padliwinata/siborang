a46f410c3f0aa4a1030c145bba58790ec9133d33
Padliwinata <rpadliwinata@gmail.com>
2020-04-23 21:43:18 +0700

    Add files via upload

---
+++ borang/flask_app.py
@@ -0,0 +1,306 @@
+from flask import Flask, session, redirect, url_for, render_template, request, flash
+from flask_sqlalchemy import SQLAlchemy
+import hashlib
+app = Flask(__name__)
+app.secret_key = "my_name"
+app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///borang.sqlite3'
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db = SQLAlchemy(app)
+
+# 80abd1c7b39878f896b4d09b68f08217f9e6d5b321db974dd5d24fc762bcb06f
+
+kriteria = [
+    'Kondisi Eksternal',
+    'Profil Unit Pengelola',
+    'Kriteria',
+    'Tata Pamong, Tata Kelola, dan Kerjasama',
+    'Mahasiswa',
+    'Sumber Daya Manusia',
+    'Keuangan, Sarana, dan Prasarana',
+    'Pendidikan',
+    'Penelitian',
+    'Luaran dan Capaian Tridharma',
+    'Analisis dan Capaian Kinerja',
+    'Analisis SWOT atau Analisis Lain Yang Relevan',
+    'Program Pengembanga',
+    'Program Keberlanjutan'
+]
+
+daftar_prodi = [
+    [
+        'S1 Teknik Telekomunikasi',
+        'S1 Teknik Telekomunikasi (Internasional)',
+        'S1 Teknik Elektro',
+        'S1 Teknik Elektro (Internasional)',
+        'S1 Teknik Fisika',
+        'S1 Teknik Komputer',
+        'S2 Teknik Elektro-Telekomunikasi'
+    ],
+    [
+        'S1 Informatika',
+        'S1 Informatika (Internasional)',
+        'S1 Teknologi Informasi',
+        'S1 Rekayasa Perangkat Lunak',
+        'S2 Informatika'
+    ],
+    [
+        'S1 Teknik Industri',
+        'S1 Teknik Industri (Internasional)',
+        'S1 Sistem Informasi',
+        'S1 Sistem Informasi (Internasional)',
+        'S1 Teknik Logistik',
+        'S2 Teknik Industri'
+    ],
+    [
+        'S1 International ICT Business',
+        'S1 Manajemen Bisnis Telekomunikasi & Informatika (MBTI)',
+        'S1 Akuntansi',
+        'S2 Manajemen'
+    ],
+    [
+        'S1 Administrasi Bisnis',
+        'S1 Administrasi Bisnis (Internasional)',
+        'S1 Ilmu Komunikasi',
+        'S1 Ilmu Komunikasi (Internasional)',
+        'S1 Digital Public Relation'
+    ],
+    [
+        'S1 Desain Komunikasi Visual',
+        'S1 Desain Komunikasi Visual (Internasional)',
+        'S1 Product Innovation & Management',
+        'S1 Desain Interior',
+        'S1 Kriya (Fashion and Textile Design)',
+        'S1 Visual Arts (Seni Rupa)',
+        'S2 Desain'
+    ],
+    [
+        'D3 Teknologi Telekomunikasi',
+        'D3 Teknik Informatika',
+        'D3 Sistem Informasi',
+        'D3 Sistem Informasi Akuntansi',
+        'D3 Teknologi Komputer',
+        'D3 Digital Marketing',
+        'D3 Perhotelan'
+    ]
+]
+
+daftar_fakultas = [
+    'Fakultas Teknik Elektro',
+    'Fakultas Teknik Informatika',
+    'Fakultas Rekayasa Industri',
+    'Fakultas Ekonomi dan Bisnis',
+    'Fakultas Komunikasi dan Bisnis',
+    'Fakultas Industri Kreatif',
+    'Fakultas Ilmu Terapan'
+]
+
+id_kriteria = [f"krit{x}" for x in range(14)]
+
+
+def encrypt_string(hash_string):
+    sha_signature = hashlib.sha256(hash_string.encode()).hexdigest()
+    return sha_signature
+
+
+class User(db.Model):
+    id_user = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    email = db.Column(db.String(50))
+    username = db.Column(db.String(50))
+    password = db.Column(db.String(64))
+    level = db.Column(db.String(14))
+    prodi = db.Column(db.String(30), nullable=True)
+
+    def __init__(self, email, username, password, level, prodi):
+        self.email = email
+        self.username = username
+        self.password = password
+        self.level = level
+        self.prodi = prodi
+
+    def __str__(self):
+        return self.username
+
+
+class Borang(db.Model):
+    id_borang = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    isi = db.Column(db.String(28))
+    username_kaprodi = db.Column(db.String(50), nullable=False)
+    prodinya = db.Column(db.String(30), nullable=False)
+
+    def __init__(self, isi, username_kaprodi):
+        self.isi = isi
+        self.username_kaprodi = username_kaprodi
+
+    def __str__(self):
+        return self.username_kaprodi
+
+
+@app.route("/login", methods=["POST", "GET"])
+def login():
+    if request.method == "GET":
+        if "status" in session:
+            if session["status"] == "active":
+                flash("You are already logged in")
+                return redirect(url_for("home"))
+            else:
+                return render_template("login.html")
+        else:
+            return render_template("login.html")
+    else:
+        username = request.form["username"]
+        password = encrypt_string(request.form["password"])
+        found_user = User.query.filter_by(username=username).first()
+        if found_user:
+            if found_user.password == password:
+                email = found_user.email
+                level = found_user.level
+                session["username"] = username
+                session["email"] = email
+                session["level"] = level
+                session["status"] = "active"
+                flash("Login success")
+                return redirect(url_for("home"))
+            else:
+                flash("Password wrong")
+                return redirect(url_for("login"))
+        else:
+            flash("Username not found")
+            return redirect(url_for("login"))
+
+
+# /akun, /soal, /penilaian, /hasil
+@app.route("/")
+def home():
+    if "status" in session:
+        if session["status"] == "active":
+            username = session["username"]
+            level = session["level"]
+            email = session["email"]
+            return render_template("home.html", username=username, level=level, email=email)
+        else:
+            flash("You are not logged in yet")
+            return redirect(url_for("login"))
+    else:
+        session["status"] = "inactive"
+        flash("You are not logged in yet")
+        return redirect(url_for("login"))
+
+
+@app.route("/logout")
+def logout():
+    session.pop("username")
+    session.pop("email")
+    session.pop("level")
+    session.pop("status")
+    flash("Logout success")
+    return redirect(url_for("login"))
+
+
+@app.route("/signup", methods=["POST", "GET"])
+def signup():
+    if request.method == "GET":
+        if "username" in session:
+            flash("You are still logged in. Please logout before sign up")
+            return redirect(url_for("home"))
+        else:
+            session["status"] = "inactive"
+            return render_template("signup.html")
+    else:
+        if request.form["stage"] == "first":
+            level = request.form["level"]
+            email = request.form["email"]
+            password = encrypt_string(request.form["password"])
+            username = request.form["username"]
+            session["email"] = email
+            session["password"] = password
+            session["username"] = username
+            session["level"] = level
+            if level == "Kaprodi":
+                return redirect(url_for("fakultas"))
+        email = session["email"]
+        username = session["username"]
+        password = session["password"]
+        level = session["level"]
+        new_user = User(email, username, password, level, '')
+        db.session.add(new_user)
+        db.session.commit()
+        flash("Signup success")
+        return redirect(url_for("login"))
+
+
+@app.route("/soal", methods=["POST", "GET"])
+def soal():
+    if request.method == "GET":
+        if "username" in session:
+            username = session["username"]
+            return render_template("soal.html", username=username, kriteria=kriteria, id_kriteria=id_kriteria)
+        else:
+            session["status"] = "inactive"
+            flash("Please login first")
+            return redirect(url_for("login"))
+    else:
+        username_kaprodi = session["username"]
+        nilai = request.form.getlist('id_kriteria')
+        if len(nilai) != 14:
+            flash("Penilaian belum lengkap, silahkan isi kembali")
+            return redirect(url_for("soal"))
+        else:
+            save = ','.join(nilai)
+            borang = Borang(save, username_kaprodi)
+            db.session.add(borang)
+            db.session.commit()
+            flash("Penilaian telah tersimpan")
+            return redirect(url_for("soal"))
+
+
+@app.route("/fakultas", methods=["POST", "GET"])
+def fakultas():
+    if request.method == "GET":
+        if "username" in session:
+            username = session["username"]
+            if User.query.filter_by(username=username).first():
+                flash("You are not allowed to access this page")
+                return redirect(url_for("home"))
+            else:
+                return render_template("fakultas.html", daftar_fakultas=daftar_fakultas)
+        else:
+            flash("You are not allowed to access this page")
+            return redirect(url_for("home"))
+    else:
+        fakultas_form = request.form["fakultas"]
+        session["fakultas"] = fakultas_form
+        return redirect(url_for("prodi"))
+
+
+@app.route("/prodi", methods=["POST", "GET"])
+def prodi():
+    if request.method == "GET":
+        if "username" in session:
+            username = session["username"]
+            if User.query.filter_by(username=username).first():
+                flash("You are not allowed to access this page")
+                return redirect(url_for("home"))
+            else:
+                fakultas = session["fakultas"]
+                urutan = [x for x, y in enumerate(daftar_fakultas) if y == fakultas][0]
+                return render_template("prodi.html", daftar_prodi=daftar_prodi[urutan])
+        else:
+            flash("You are not allowed to access this page")
+            return redirect(url_for("home"))
+    else:
+        prodi_form = request.form["prodi"]
+        session["prodi"] = prodi_form
+        email = session["email"]
+        username = session["username"]
+        password = session["password"]
+        level = session["level"]
+        new_user = User(email, username, password, level, prodi_form)
+        db.session.add(new_user)
+        db.session.commit()
+        flash("Signup success")
+        return redirect(url_for("login"))
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+    db.create_all()
